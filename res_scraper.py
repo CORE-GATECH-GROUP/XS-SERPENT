0a6ffead7ff0383f7ec05581aa6415b89ce8665d
drewejohnson <1drew.e.johnson@gmail.com>
2017-03-27 22:01:31 -0400

    SERPENT resfile class instance
    After thinking about all the extra functions we will have to incorporate
    into the _res.m files, I am in the progress of making this a class
    object. This will allow easier read/write and eventually plot commands
    without having to pass a lot of data directly back and forth
    
    Need to update the documentation, and this is really preliminary at this
    point
    
    Signed-off-by: drewejohnson <1drew.e.johnson@gmail.com>

--- res_scraper.py
+++ res_scraper.py
@@ -7,6 +7,7 @@ A. Johnson
   
 """
 from os.path import exists
+from datetime import datetime
 import numpy as np
 import messages
 from mat2py import vec2list
@@ -14,7 +15,42 @@ from mat2py import vec2list
 validBurntypes = ('BURN_STEP', 'BURNUP', 'DAYS')
 
 
-def res_scraper(resfile, gculist, varlist, burnlist, burntype, nfg=None):
+class SerpResFile(object):
+    """SERPENT _res.m output object
+    
+    Class for extracting, printing, and (eventually) plotting group constant values 
+        from SERPENT output files 
+    """  # TODO: Fill this out better
+
+    def __init__(self, resfile: str, process: bool, gculist: list, varlist: list, burnlist: list, burntype: str):
+        """
+        
+        :param resfile: 
+        :param process: 
+        :param gculist:
+        :param varlist: 
+        :param burnlist: 
+        :param burntype: 
+        """
+        self.resfile = resfile
+        self.gculist = gculist
+        self.varlist = varlist
+        self.burnlist = burnlist
+        self.burntype = burntype
+        if process:
+            self.gcuvals = res_scraper(resfile, gculist, varlist, burnlist, burntype)
+            self.time = str(datetime.now())
+        else:
+            self.time = None
+            self.gcuvals = {gcu: None for gcu in gculist}
+            # purpose of this section is to define the variables here, but then have a method later on
+            # that can write the contents of this file in a syntax for easy rereading without reprocessing
+            # i.e. having a file that declares an instance of this class, with a given list of variables, but then
+            # defines those variables directly
+            # time should be read in from external file and written everytime the output files are written
+
+
+def res_scraper(resfile, gculist, varlist, burnlist, burntype):
     """
     Parse the _res.m file and return a dictionary where the keys are specified ` constant universes
     and the values are matrices of the desired variables at the desired points.
@@ -27,9 +63,6 @@ def res_scraper(resfile, gculist, varlist, burnlist, burntype, nfg=None):
     :param burnlist: List of burnup points (can be given in days of in burnup as specified by burntype)
     :param burntype: One of three values indicating what burnup values to look for
         BURN_STEP: unitless burnup point, BURNUP: value of burnup in MWd/kgU, BURN_DAYS: day in the burnup cycle
-    :param nfg: Number of collapsed energy groups returned. Also the length of the group constant vectors
-        If given, the matrices to be returned can be generated immediately. Otherwise, must wait until the number 
-        of energy groups can be determined
     :return: gcu_vals dictionary where the keys are strings corresponding to the group constant universes, and the 
         corresponding values are the matrices gcu_vals[v, b, e]
         For errors:
@@ -46,10 +79,7 @@ def res_scraper(resfile, gculist, varlist, burnlist, burntype, nfg=None):
         return -2
     # TODO add some potential error handling for non-positive integer values of nfg
 
-    if nfg is None:
-        gcu_vals = {gcu: None for gcu in gculist}
-    else:
-        gcu_vals = {gcu: np.zeros((len(varlist), len(burnlist), nfg), dtype=float) for gcu in gculist}
+    gcu_vals = {gcu: None for gcu in gculist}
 
     with open(resfile, 'r') as res:
         line = res.readline()
