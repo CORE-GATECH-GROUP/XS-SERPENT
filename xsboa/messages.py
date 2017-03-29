f7ca5d640a5676dc84be64efe677017920265db7
drewejohnson <1drew.e.johnson@gmail.com>
2017-03-29 10:18:17 -0400

    Package style folder structure; setup.py
    In order to make this project something that can be installed simply and
    consistently, I made a few slightly large structural changes:
      - moved all the python files for this project into the directory xsboa
      - created a setup.py file
      - added __init__.py to the xsboa directory
    
    The setup.py file allows the project to be installed with the command
      python setup.py install
    Doing this compiles all the necessary modules so the submodules and subfunctions
    can be imported from any directory like a normal python package
    
    The __init__.py file in the xsboa directory essentially tells the import
    command what modules and functions are valid to be imported. On this
    branch, only messages is present, but down the road modules like scraper
    and any plotting and preparation files will be included here as well.
    
    Signed-off-by: drewejohnson <1drew.e.johnson@gmail.com>

--- xsboa/messages.py
+++ xsboa/messages.py
@@ -1,5 +1,4 @@
 """
-
 Messages
     - Warnings, Errors, and Status Updates
 
@@ -9,62 +8,65 @@ Functions:
     - warn: Produce a simple warning message for a thing that denotes inspection
     - fatal: Produce a descriptive fatal error message
     - status: Produce a status update
-    -oprint: Print to stdout or write to output file
+    - oprint: Print to stdout or write to output file
 """
 lineBreak = '-' * 40
 
 
-def oprint(args, msg):
+def oprint(msg: str, output):
     """
     Write message to stdout (screen) or append to output file
-    :param args: arguments from argparser in main file
-        needs args.output
-        If args.output is none, print to stdout
-        Else, append to file found at args.output
+    :param output: Either file string to append to or None
+        If none, print to string
     :param msg: Message to write
     :return: None
     """
-    if args.output is None:
+    if output is None:
         print(msg)
         return None
     else:
-        with open(args.output, 'a') as out:
+        with open(output, 'a') as out:
             out.write(msg)
         return None
 
 
-def status(msg_, verbose_):
+def status(msg_: str, args: dict):
     """
     Print a simple status update if verbose parameter is true
     :param msg_: Message
-    :param verbose_: Verbose flag (boolean)
+    :param args: Dictionary of input arguments
+        - requires keys 'verbose': Bool and 'output': (None, str)
     :return: None
     """
-    if verbose_:
-        print(msg_)
+    if args['verbose']:
+        oprint(msg_, args['output'])
     return None
 
 
-def warn(msg_, loc_=None):
+def warn(msg_: str, loc_, args: dict):
     """
     Print a warning message
     :param msg_: Message
-    :param loc_: Location of the warning message (optional)
+    :param loc_: Location of the warning message
+    :param args: Dictionary of input arguments
+        - Requires key 'output' : (None or file string)
     :return: None
     """
-    print('Warning: ', msg_)
+    oprint('Warning: ' + msg_, args['output'])
     if loc_ is not None:
-        print(' in ', loc_)
+        oprint(' in ' + loc_, args['output'])
     return None
 
 
-def fatal(msg_, loc_):
+def fatal(msg_, loc_: str, args: dict):
     """
     Print a descriptive message for a fatal error. Cease operation
     :param msg_: Desciptive message about what went wrong
     :param loc_: Function that called this error message
+    :param args: Dictionary of input arguments
+        - Requires key 'output' : (None or file string)
     :return: None
     """
     f_str = 'Error: ' + msg_ + '\n in: ' + loc_
-    print(lineBreak + '\n' + f_str + '\n' + lineBreak)
+    oprint(lineBreak + '\n' + f_str + '\n' + lineBreak, args['output'])
     raise SystemError(f_str)
