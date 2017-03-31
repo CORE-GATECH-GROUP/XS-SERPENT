0ac37195db40d452a78ed540e4510882849174ec
drewejohnson <1drew.e.johnson@gmail.com>
2017-03-31 14:42:20 -0400

    Initial read-param commit
    Signed-off-by: drewejohnson <1drew.e.johnson@gmail.com>

---
+++ xsboa/readparam.py
@@ -0,0 +1,11 @@
+"""
+readparam
+    - retrieve relevant parameters from data block
+
+A. Johnson    
+Functions:
+    - readparam(): take in the original SERPENT input file with data block, and return important parameters
+    
+"""
+
+from xsboa import messages
