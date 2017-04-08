914a9bc08bd65acb40264411bee6eeb7c63a0269
Vedant Mehta <vmehta31@gatech.edu>
2017-04-08 16:34:37 -0400

    Partially complete code with Error missing - Vedant

--- xsboa/burnup_convert.py
+++ xsboa/burnup_convert.py
@@ -25,3 +25,28 @@
             I.e3. burn badburn 0.1 0.2 ....
             O.e3. <FATAL ERROR> no badburn in SERPENT syntax
 """
+
+
+inputStr = 'burn daytot 1 5 10 25 50'
+
+burnupTypes = ['bustep', 'butot', 'daystep', 'daytot', 'decstep', 'dectot']
+splittedString = str.split(inputStr)
+if splittedString[0]=='burn':
+    if splittedString[1] in burnupTypes:
+        print('dep ', splittedString[1])
+
+        for i in range(2, len(splittedString)):
+
+            if 'R' in splittedString[i]:
+                 splittedChar = splittedString[i].split('R')
+                 for j in range(0, int(splittedChar[1])):
+                     print(splittedChar[0])
+            elif any(c.isalpha() for c in splittedString[i]):
+                print('ERROR')
+            else:
+                print(splittedString[i])
+    else:
+        print('ERROR')
+
+else:
+    print('ERROR')
