dd5c1891d1ffecf333fe70ec022439e0b0e9bbd1
drewejohnson <1drew.e.johnson@gmail.com>
2017-04-28 14:55:45 -0400

    Updated readparam - burnup string in branch dictionary; Cleaned docstring
    The burnup string, if specified, is added to the corresponding branch, rather than an integer pointing to an index in another vector. That's silly. Updated the testing accordingly.
    
    exe_str should be setup to allow for use of the .format() method, where the only argument is the filename i.e. ./sss2 {0} > {0}.o
    
    Cleaned up some docstrings to make things look a little cleaner/comply with some PEP8 standards. These changes should help with documentation later.
    
    Signed-off-by: drewejohnson <1drew.e.johnson@gmail.com>

--- testing/test_readparam.py
+++ testing/test_readparam.py
@@ -1,9 +1,8 @@
 """
 
-test_readparam.py
-    - Testing for the parameter reader function
+test_readparam.py - Testing for the parameter reader function
     
-A. Johnson
+Andrew Johnson
 
 Test should be ran from the same directory as the setup file (home directory)
 
@@ -21,53 +20,54 @@ class TestReadParam(unittest.TestCase):
     def setUp(self):
         # Intended outputs
         self.loc = (0, 21)
-        self.nom = {'UO2': {'temp': 900, 'adens': 6.5850E-02}, 'MOX1': {'temp': 900}, 'MOX2': {'temp': 900},
+        self.nom = {'UO2': {'temp': 900, 'adens': 6.5850E-02},
+                    'MOX1': {'temp': 900}, 'MOX2': {'temp': 900},
                     'MOX3': {'temp': 900},
-                    'water1': {'mdens': 7.088200E-02, 'temp': 600}}
-        self.exe = 'qsub -v INPARG={} serp.pbs'
-        self.branch = {'hUO2': {'UO2': {'temp': 1200}}, 'hFuel': {'UO2': {'temp': 1200}, 'MOX1': {'temp': 1200},
-                                                                  'MOX2': {'temp': 1200}, 'MOX3': {'temp': 1200}},
-                       'void40b0': {'water1': {'void': 40}, 'burn': 1}}
-        self.burn = ['burn 1 4 5R5 keep 1 30', 'burn 1 4 5R5 keep 1 30']
+                    'water1': {'mdens': 7.088200E-02, 'temp': 600},
+                    'burn': 'burn 1 4 5R5 keep 1 30'}
+        self.exe = 'qsub -v INPARG={0} serp.pbs'
+        self.branches = {'hUO2': {'UO2': {'temp': 1200}},
+                         'hFuel': {'UO2': {'temp': 1200},
+                                   'MOX1': {'temp': 1200},
+                                   'MOX2': {'temp': 1200},
+                                   'MOX3': {'temp': 1200}},
+                         'void40b0': {'water1': {'void': 40},
+                                      'burn': 'burn daystep 1 4 5R5 keep 1 30'}}
         self.var = ['INF', 'TOX', 'INF_S0']
 
         # Actual outputs
-        self.aloc, self.anom, self.obranch, self.aburn, self.aexe, self.avar = readparam(inpfile, args)
+        self.aloc, self.anom, self.abranches, self.aexe, self.avar = readparam(inpfile, args)
 
     def test_no_file(self):
-        """Validate output if the file does not exist (False, -2)."""
-        b_loc, dum1, dum2, dum3, dum4, dum5 = readparam('bad', args)
-        self.assertEqual(b_loc, (False, -1))
+        """Validate output if the file does not exist."""
+        vals = readparam('bad', args)
+        self.assertEqual(vals[0], (False, -1))
 
     def test_no_start(self):
-        """Vallidate output if a file exists but param block start is missing (True, -2)."""
-        b_loc, dum1, dum2, dum3, dum4, dum5 = readparam(os.path.join('testing', 'title.txt'), args)
-        self.assertEqual(b_loc, (True, -2))
+        """Validate output if a file exists but param block start is missing."""
+        vals = readparam(os.path.join('testing', 'title.txt'), args)
+        self.assertEqual(vals[0], (True, -2))
 
     def test_no_end(self):
-        """Validate output if the file exists but param block end is missing (<int>, -1)."""
+        """Validate output if the file exists but param block end is missing."""
         with open(os.path.join('testing', 'title.txt'), 'r') as title:
             tlines = title.readlines()
         with open(os.path.join('testing', 'title_paramStart.txt'), 'w') as paramStart:
             paramStart.writelines(tlines)
             paramStart.write('/* xsboa start\n')
-        b_loc, dum1, dum2, dum3, dum4, dum5 = readparam(os.path.join('testing', 'title_paramStart.txt'), args)
+        vals = readparam(os.path.join('testing', 'title_paramStart.txt'), args)
         os.remove(os.path.join('testing', 'title_paramStart.txt'))
-        self.assertEqual(b_loc[1], -1)
+        self.assertEqual(vals[0][1], -1)
 
     def test_loc(self):
         """Validate location of parameter block - zero indexed."""
         self.assertEqual(self.aloc, self.loc)
 
-    def test_burn(self):
-        """Validate burnup list."""
-        self.assertEqual(self.aloc, self.loc)
-
     def test_branch(self):
         """Validate branching dictionary."""
-        for branch in self.branch:
+        for branch in self.branches:
             with self.subTest(msg='branch {}'.format(branch)):
-                self.assertEqual(self.branch[branch], self.obranch[branch])
+                self.assertEqual(self.branches[branch], self.abranches[branch])
 
     def test_exe(self):
         """Validate execution string."""
--- xsboa/readparam.py
+++ xsboa/readparam.py
@@ -2,39 +2,56 @@
 readparam
     - retrieve relevant parameters from data block
 
-A. Johnson    
+Andrew Johnson
+    
 Functions:
-    - readparam(): take in the original SERPENT input file with data block, and return important parameters
-    - paramsplit(): take in a split parameter line and return a dictionary with parameter: value pairs
+
+    - readparam(): take in the original SERPENT input file with data 
+    block, and return important parameters
+    - paramsplit(): take in a split parameter line and return a 
+    dictionary with parameter: value pairs
+
 Notes:
-    Input file should be a valid SERPENT input file with the data block encompased in C/C++ style block comments
-    with xsboa in each line 
-    i.e. /* start xsboa .... <data> ... stop xsboa */ 
-    or /* xsboa ... <data> ... xsboa */
+    
+    Input file should be a valid SERPENT input file with the data block 
+    encompased in C/C++ style block comments with xsboa in each line ::
+    
+        /* start xsboa .... <data> ... stop xsboa */ 
+        /* xsboa ... <data> ... xsboa */
     
     Nominal material properties are given on each line in the form 
-      nom <mName> <p0 v0 p1 v1... pN vN> where pn is the specific parameter and vN is the nominal value
-    Not all materials must have nominal conditions, nor are nominal conditions required.
+    ``nom <mName> <p0 v0 p1 v1... pN vN>`` where pn is the specific 
+    parameter and vN is the nominal value.
+    Not all materials must have nominal conditions, nor are nominal 
+    conditions required.
     
     Branch calculations can be multiline and stop when:
-      - the first word in the next line is branch or nom, or 
-      - the next line is is empty
-    Branch syntax is similar to the nominal condition syntax, where perturbed conditions for each material are given
-    on a line.
-    ex.
+    
+        the first word in the next line is branch or nom, or 
+        the next line is is empty
+        
+    Branch syntax is similar to the nominal condition syntax, where 
+    perturbed conditions for each material are given on a line. ::
+
         branch hUO2 UO2 temp 1200
         branch hFuel MOX1 temp 1200
         MOX2 temp 1200
         MOX3 temp 1200
         UO2 temp 1200
     
-    Two branches are defined here. The first, named hUO2 indicates that the temperature of material 'UO2' should be 
-    changed to 1200K. The second, hFuel, changes the temperature for four materials to 1200K
+    Two branches are defined here.  The first, named hUO2 indicates 
+    that the temperature of material 'UO2' should be changed to 1200K. 
+    The second, hFuel, changes the temperature for four materials to 1200K
     
     Parameters that can be perturbed at this point:
-        temp, dens (adens or mdens), and void (specify the void fraction so new density $\rho' = \rho(1-vf)$
     
-    The nominal burnup schedule, if given, will be the first line with 'burn' as the first word. 
+        #. temperature 
+        #. density (adens or mdens)
+        #. void  (specify the void fraction so new density 
+        :math:`\rho' = \rho(1-vf)`
+    
+    The nominal burnup schedule, if given, will be the first line with 
+    'burn' as the first word. 
     
 """
 import os
@@ -45,14 +62,16 @@ import xsboa
 def paramsplit(lsplit: (list, tuple), start_indx: int, lcount: int, args: dict):
     """Convert a xsboa parameter line into a dictionary of parameter value pairs
     Starting at the index of the first parameter, the pattern is as follows: 
-    ['p0', 'v0', 'p1', 'v1', ...]
+    ``['p0', 'v0', 'p1', 'v1', ...]``
     Each value v<n> will be converted to float. If not, a fatal error is raised
     :param lsplit: List/tuple of line that has been split
     :param start_indx: Index of the first parameter 
     :param lcount: current location in file (used in fatal error)
     :param args: Argument dictionary
-        - ;file': input file
-        - 'output' as key and either None or output file as value
+        
+        'file': input file
+        'output' as key and either None or output file as value
+        
     :return: dictionary of {'p0': v0, 'p1': v1, ...}
     """
     params = {}
@@ -73,23 +92,22 @@ def readparam(infile: str, args=None):
     :param args: Additional arguments for printing status and error messages
         If not given, defaults to status updates given and printing to screen
     :return: 
+        
         locs - tuple of start and end indices of data block
             - Note: Will be zero indexed i.e. line 1 is read as line 0
         nom_d - dictionary of materials and their nominal conditions
             None if no nominal conditions given
         branch_d - dictionary of branch names with perturbed parameters as values
-            If a perturbed branch has burnup, then a key 'burn': indx is added, where indx points to 
-            the string in burnstrings that should be used to build this branches burnup
-        burnstrings: list of strings corresponding to burnup values with 
-            burnstrings[0] corresponding to the nominal branch (could be empty '')
-            all other items are burnup regimes for branch states (could not exist)
-        exe_str: Formattable string to call for execution of the SERPENT input files
-            ex: './sss2 {} > {}.o'
+            If a perturbed branch has burnup, then a key ``'burn': <burnstring>`` is added, 
+            where ``<burnstring>`` indicates the freeform burnup schedule
+        exe_str - Formatable string to execute ``SERPENT`` runs. The format argument
+            will be the filename. ``./sss2 {0} > {0}.o``
         var_l: List of variables/keywords to be expanded and then scraped in res_scraper
             - Will not be used for creation and execution of SERPENT input files
             - Should not be none for processing. Or else what is the point?
             
     For errors:
+    
         locs = (False, -1) -> Could not open file
         locs = (<block start>, -1) -> Read through file, but could not find end of xsboa block
         locs = (True, -2) -> Read through file, but could not find start of xsboa block 
@@ -99,14 +117,13 @@ def readparam(infile: str, args=None):
 
     nom_d = {}
     branch_d = {}
-    burnstrings = ['', ]
     exe_str = ''
     var_l = []
 
     if not os.path.exists(infile):
         xsboa.messages.warn('File {} does not exist and cannot be processed'.format(os.path.join(os.getcwd(), infile)),
                             'readparam()', args)
-        return (False, -1), nom_d, branch_d, burnstrings, exe_str, var_l
+        return (False, -1), nom_d, branch_d, exe_str, var_l
 
     with open(infile, 'r') as inobj:
         line = inobj.readline()
@@ -126,7 +143,7 @@ def readparam(infile: str, args=None):
                         if lsplit[0] == 'nom':
                             nom_d[lsplit[1]] = paramsplit(lsplit, 2, lcount + 1, args)
                         elif lsplit[0] == 'burn':
-                            burnstrings[0] = line.strip()
+                            nom_d['burn'] = line.strip()
                         elif lsplit[0] == 'exe_str':
                             exe_str = ' '.join(lsplit[1:])
                         elif lsplit[0] == 'var':
@@ -144,8 +161,7 @@ def readparam(infile: str, args=None):
                                     'nom', 'branch', 'exe_str') and '*/' not in line:
                                 lsplit = line.split()
                                 if lsplit[0] == 'burn':
-                                    branch_d[branch_name]['burn'] = len(burnstrings)
-                                    burnstrings.append(line.strip())
+                                    branch_d[branch_name]['burn'] = line.strip()
                                 else:
                                     branch_d[branch_name][lsplit[0]] = paramsplit(lsplit, 1, lcount + 1, args)
                                 line = inobj.readline()
@@ -154,15 +170,24 @@ def readparam(infile: str, args=None):
                         line = inobj.readline()
                         lcount += 1
                     if line == '':
-                        return (block_start, block_end), nom_d, branch_d, burnstrings, exe_str, var_l
+                        return (block_start, block_end), nom_d, branch_d, exe_str, var_l
                 block_end = lcount
                 xsboa.messages.status('  done', args)
-                return (block_start, block_end), nom_d, branch_d, burnstrings, exe_str, var_l
+                return (block_start, block_end), nom_d, branch_d, exe_str, var_l
             line = inobj.readline()
             lcount += 1
-        return (True, -2), nom_d, branch_d, burnstrings, exe_str, var_l
+        return (True, -2), nom_d, branch_d, exe_str, var_l
 
 
 # testing
 if __name__ == '__main__':
-    locs, nom, branch, burns = readparam(os.path.join('testing', 'uo2mox.txt'), None)
+    locs, nom, branches, exe, var_lst = readparam(os.path.join('testing', 'uo2mox.txt'), None)
+    print(locs)
+    print('--')
+    for each in nom:
+        print(each, nom[each])
+    print('--')
+    for branch in branches:
+        print(branch, branches[branch])
+    print('--\n', exe)
+    print('--\n', var_lst)
